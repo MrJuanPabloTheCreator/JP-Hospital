55e07dc16c639886c8bd82881308275a8d00a753
MrJuanPabloTheCreator <juanpablocerdasilva@gmail.com>
2024-02-17 17:47:59 -0600

    Navbar Icons changes

--- app/(pages)/components/navbarcomp/topNav.tsx
+++ app/(pages)/components/navbarcomp/topNav.tsx
@@ -1,22 +1,23 @@
-import { Github, Linkedin, User } from "lucide-react"
+import { User } from "lucide-react"
+import { FaLinkedin, FaGithub, FaUserCircle } from "react-icons/fa";
 import Link from "next/link"
 
 const TopNavbar = () => {
     
     return (
-        <div className="flex justify-between bg-white text-blue-950 font-bold py-5 xl:mx-20 lg:mx-10 mx-5">
+        <div className="flex justify-between bg-white text-blue-950 font-bold py-3 xl:mx-20 lg:mx-10 mx-5">
             <Link href={'/'} className="flex items-center text-3xl">
                 <p className="text-pink-700 mr-1">JP</p> Hospital
             </Link>
             <div className="grid grid-cols-3 gap-2">
                 <Link className="hover:text-pink-700" href={'https://www.linkedin.com/in/juanpcerda/'}>
-                    <Linkedin size={'36px'}/>
+                    <FaLinkedin size={'36px'}/>
                 </Link>
                 <Link className="hover:text-pink-700" href={'https://github.com/MrJuanPabloTheCreator'}>
-                    <Github size={'36px'}/>
+                    <FaGithub size={'36px'}/>
                 </Link>
                 <Link className="hover:text-pink-700" href={'https://main.d1wqsrgpxi6f4p.amplifyapp.com/'}>
-                    <User size={'36px'}/>
+                    <FaUserCircle size={'36px'}/>
                 </Link>
             </div>
         </div>
